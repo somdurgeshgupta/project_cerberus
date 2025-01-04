090dedbaa4ae1f8455fe2690fb2acf46660e6a7f
somdurgeshgupta <somdurgeshgupta@gmail.com>
2025-01-05 00:50:01 +0530

    forgot password

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -6,26 +6,35 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-forgot-password',
   standalone: false,
-  
+
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.css'
+  styleUrl: './forgot-password.component.css',
 })
 export class ForgotPasswordComponent {
-
   forgotpasswordForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
-      this.forgotpasswordForm = this.fb.group({
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]]
-      });
-      
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/dashboard']);
     }
-  
-    onSubmit(){
-      this.authService.forgotpassword(this.forgotpasswordForm.value).subscribe((res:any)=>{
-        console.log(res);
-      })
-    }
+    this.forgotpasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
+  onSubmit() {
+    this.authService
+      .forgotpassword(this.forgotpasswordForm.value)
+      .subscribe((res: any) => {
+        if (res.success) {
+          alert(res.message);
+          this.router.navigate(['']);
+        }
+      });
+  }
 }
