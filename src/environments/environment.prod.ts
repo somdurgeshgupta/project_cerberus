827838b27cf827428ec54cda087a26a6156b8317
somdurgeshgupta <somdurgeshgupta@gmail.com>
2025-01-06 23:41:58 +0530

    updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL : 'http://13.203.62.204/api/v1/',
+    API_URL : 'http://13.203.62.204:4200/api/v1/',
   };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     // API_URL : 'http://192.168.29.208:3000/api/v1/',
-    API_URL : 'http://13.203.62.204/api/v1/',
+    API_URL : 'http://13.203.62.204:4200/api/v1/',
   };
   
