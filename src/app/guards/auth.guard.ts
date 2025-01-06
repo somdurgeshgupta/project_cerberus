5106093a14c95e9cd9f1e9c9976e5293939b45e5
somdurgeshgupta <somdurgeshgupta@gmail.com>
2025-01-06 16:26:32 +0530

    added route

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -10,7 +10,7 @@ export const authGuard: CanActivateFn = (route, state) => {
 
   const isLoggedIn = authService.isLoggedIn();
   if (!isLoggedIn) {
-    router.navigate(['/login']);
+    router.navigate(['/']);
   }
   return isLoggedIn;
 };
