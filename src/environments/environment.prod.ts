f56798e7436add9bc5ce2c4d938b81efb81ea1a8
ejdeathcrash <ejdeathcrash@gmail.com>
2025-03-12 23:02:57 +0530

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_URL : 'https://172.31.12.84:3000/api/v1/',
+    API_URL : 'https://13.126.195.144:3000/api/v1/',
     // API_URL : "https://cerberus-backend.onrender.com/api/v1/",
     GOOGLE_AUTH_KEY: "855831171805-hbharbt1j31v67i2ruve5trh2pa50blb.apps.googleusercontent.com"
   };
